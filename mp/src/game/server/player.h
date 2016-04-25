e03dd6aeefff3ca4950aecfe103906e22b8348d6
Jordon Moss <mossj32@gmail.com>
2016-04-25 04:26:14 -0300

    Moved StepSoundCache back to private in player.h

--- mp/src/game/server/player.h
+++ mp/src/game/server/player.h
@@ -1204,6 +1204,15 @@ private:
 
 	bool m_autoKickDisabled;
 
+	struct StepSoundCache_t
+	{
+		StepSoundCache_t() : m_usSoundNameIndex(0) {}
+		CSoundParameters	m_SoundParameters;
+		unsigned short		m_usSoundNameIndex;
+	};
+	// One for left and one for right side of step
+	StepSoundCache_t		m_StepSoundCache[2];
+
 	CUtlLinkedList< CPlayerSimInfo >  m_vecPlayerSimInfo;
 	CUtlLinkedList< CPlayerCmdInfo >  m_vecPlayerCmdInfo;
 
@@ -1217,15 +1226,6 @@ private:
 
 public:
 	virtual unsigned int PlayerSolidMask( bool brushOnly = false ) const;	// returns the solid mask for the given player, so bots can have a more-restrictive set
-
-	struct StepSoundCache_t
-	{
-		StepSoundCache_t() : m_usSoundNameIndex(0) {}
-		CSoundParameters	m_SoundParameters;
-		unsigned short		m_usSoundNameIndex;
-	};
-	// One for left and one for right side of step
-	StepSoundCache_t		m_StepSoundCache[2];
 };
 
 typedef CHandle<CBasePlayer> CBasePlayerHandle;
